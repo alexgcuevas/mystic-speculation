0e0f219e29843d71b1ab714e4c66a3bab4034f47
Alex Cuevas <alexgcuevas@gmail.com>
2018-11-15 21:06:33 -0800

    added connect mystic straight to query

--- query.py
+++ query.py
@@ -3,7 +3,6 @@ import pandas as pd
 import json, requests, pickle
 from sqlalchemy import create_engine
 import psycopg2
-from scrape.scraper import connect_mystic
 
 def get_recent_price(card_row, version=2):
     # Card attributes on which to query
@@ -39,6 +38,28 @@ def write_recent_prices(cards_df, rarities):
     for rarity in rarities:
         filled_df = fill_recent_prices(cards_df[cards_df['rarity']==rarity])
         filled_df.to_csv(path_or_buf='data/all_vintage_cards-{}_recent.csv'.format(rarity))
+
+def connect_mystic():
+    '''
+    Connects to mystic-speculation database, returns connection object
+    Output:
+        SqlAlchemy PostgreSQL connection object to mystic speculation database
+    '''
+    # Define database info
+    hostname = 'mystic-speculation.cwxojtlggspu.us-east-1.rds.amazonaws.com'
+    port = '5432'
+    dbname = 'mystic_speculation'
+
+    # load username and pw information for database
+    with open('scrape/login.txt', 'r') as login_info:
+        username = login_info.readline().strip()
+        password = login_info.readline().strip()
+
+    # connect to database with sqlalchemy engine
+    db_string = 'postgres://{0}:{1}@{2}:{3}/{4}'.format(username, password, hostname, port, dbname)
+    engine = create_engine(db_string)
+    connection = engine.connect()
+    return connection
 
 if __name__ == "__main__":
     cards_df = pd.read_csv('data/all_vintage_cards.csv')
