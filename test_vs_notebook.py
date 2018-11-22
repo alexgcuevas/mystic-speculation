399febd463ca260fc14b1fd6f3818f8b71e21ea0
Alex Cuevas <alexgcuevas@gmail.com>
2018-11-22 15:14:26 -0800

    got seasonal average prices working!!!!!!!!!!!!!!!

--- test_vs_notebook.py
+++ test_vs_notebook.py
@@ -14,6 +14,8 @@ import psycopg2
 # Import my own stuff
 from scrape.scraper import *
 
+
+""" Testing plotting over time """
 connection = connect_mystic()
 
 # Plots Mythic history
@@ -49,3 +51,45 @@ plot_price_history('mythic')
 plot_price_history('rare')
 plot_price_history('uncommon')
 plot_price_history('common')
+
+""" Loading standard seasonality into database """
+
+seasons_df = pd.read_csv("data/set_seasonality.csv")
+dates_df = pd.read_csv("data/set_dates.csv")
+seasons_df.fillna(0, inplace=True)
+seasons_df.head()
+dates_df.head()
+
+"""
+card set timestamp price 
+"""
+
+def avg_price_by_season(seasons, tablename):
+    connection = connect_mystic()
+    seasons_df = pd.DataFrame(columns=['cardname','setname'])
+    for season in seasons:
+        print(season)
+        # to timestamp
+        start = str(int(pd.Timestamp(season[0]).value/c))
+        end = str(int(pd.Timestamp(season[1]).value/c))
+        query = ("select cardname, setname, avg(price) as s{3}_avg "
+                 "from {0} "
+                 "where cast(timestamp as float)/1000 > {1} and cast(timestamp as float)/1000 < {2} "
+                 "group by cardname, setname ").format(tablename,start,end,season[2])
+        season_df = pd.read_sql(query, connection)
+        print(season_df.head(1))
+        seasons_df = seasons_df.merge(season_df, on=['cardname','setname'], how='outer')
+        print("seasons shape: {}".format(seasons_df.shape))
+    connection.close()
+    return seasons_df
+
+rarity = 'mythic'
+tablename = rarity+"_price_history_2"
+seasons = np.array(pd.read_csv("data/season_dates.csv"))
+mythics_by_season = avg_price_by_season(seasons, tablename)
+mythics_by_season.head()
+
+    #Upgrade avg to time-weighted average after MVP working
+    #try median?
+
+
